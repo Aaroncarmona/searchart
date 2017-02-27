2043e2490e5d2607823baa970a341f3b8609bb76
Aaron carmona <carmonespi@gmail.com>
2017-02-27 01:35:11 -0600

    Revisar
    Se guarda pero se tiene que revisar
    el tindex se tiene que cambiar por completo para ya empezar a hacer los
    formularios y como se accedera al sitio

--- vista/index.php
+++ vista/index.php
@@ -1,7 +1,21 @@
 <?php
-	include('contenedores/estatico/head.html');
-	include('contenedores/estatico/nav.html');
-	include('contenedores/dinamico/barra.php');
-	include('contenedores/dinamico/contenido.php');
-	include('contenedores/estatico/footer.html');
+	if(isset($_REQUEST['sesion'])){ //se verifica que tipo de usuario esta entrando al sistema
+		if ($_REQUEST['sesion'] == "admin") {
+			include('contenedores/estaticoadm/head.html');
+			include('contenedores/estaticoadm/nav.html');
+			include('contenedores/estaticoadm/contenedor.php');
+			include('contenedores/estaticoadm/footer.html');
+			
+		
+		}
+
+	}else{
+		include('contenedores/estatico/head.html');
+		include('contenedores/estatico/nav.html');
+		include('contenedores/dinamico/barra.php');
+		include('contenedores/dinamico/contenido.php');
+		include('contenedores/estatico/footer.html');
+	}
+
+	
 ?>
